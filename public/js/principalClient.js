12bda6576de211bf23af3b60a517ff0986df8f7b
Susana Feng Liu <fengsussy@gmail.com>
2025-04-19 00:49:43 -0600

    Un arreglo

--- public/js/principalClient.js
+++ public/js/principalClient.js
@@ -129,45 +129,6 @@ function obtenerFilaSeleccionada() {
     }
 }
 
-//Restringe la a que solo 1 checkbox este seleccionado
-function assignEvtCheckbox() {
-    document.querySelectorAll(".fila-checkbox").forEach(checkbox => {
-        checkbox.addEventListener("change", function() {
-            document.querySelectorAll(".fila-checkbox").forEach(cb => {
-                if (cb !== this) {
-                    cb.checked = false; // Desmarca los otros checkboxes
-                }
-            });
-            let empleadoSeleccionado = obtenerFilaSeleccionada();
-            localStorage.setItem('empleado', JSON.stringify(empleadoSeleccionado[1]));
-            //console.log("Local storage", localStorage.getItem('empleado'));
-            return empleadoSeleccionado;
-        });
-    });
-}
-
-// Devuelve el nombre del empleado que ha sido seleccionado de la tabla
-function obtenerFilaSeleccionada() {
-    // Buscar el checkbox que está marcado
-    const checkboxSeleccionado = document.querySelector(".fila-checkbox:checked");
-    
-    if (checkboxSeleccionado) {
-        // Obtener la fila (`tr`) que contiene el checkbox
-        const fila = checkboxSeleccionado.closest("tr");
-
-        // Extraer los valores de las celdas (`td`)
-        const datosFila = Array.from(fila.children).map(td => td.textContent.trim());
-
-        console.log("Fila seleccionada:", datosFila);
-        return datosFila; // Devuelve los datos de la fila en forma de array
-    } else {
-        localStorage.removeItem('empleado');
-        console.log("No hay ninguna fila seleccionada.");
-        return null;
-    }
-}
-
-
 //Modificar atributo 'EsActivo' del empleado en la BD por 0 para eliminarlo
 async function eliminarAfirmado(username,IpAdress,nombre){
     try {
